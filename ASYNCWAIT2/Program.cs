edde0bc5e7280d75d345aeb2dbaea27ee57a5e01
parag <parag@DESKTOP-I2Q9QKQ>
2021-09-07 22:10:55 +0530

    Add project files.

---
+++ ASYNCWAIT2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace ASYNCWAIT2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await callMethod();
+            Console.ReadKey();
+        }
+
+        public static async Task callMethod()
+        {
+            Method2();
+            var count = await Method1();
+            Method3(count);
+        }
+
+        public static async Task<int> Method1()
+        {
+            int count = 0;
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine(" Method 1");
+                    count += 1;
+                }
+            });
+            return count;
+        }
+
+        public static void Method2()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine(" Method 2");
+            }
+        }
+
+        public static void Method3(int count)
+        {
+            Console.WriteLine("Total count is " + count);
+        }
+    }
+   
+
+    
+}
